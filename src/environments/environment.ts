40b4b25db387530fe33e8427b43ea4e60bad210a
Khaffou Khaffou Mohamed <khaffoum@gmail.com>
2024-02-18 18:51:49 +0100

    changed env vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,21 @@
 export const environment = {
   production: false,
   firebase: {
-    projectId: 'films-2f0bc',
+    /* projectId: 'films-2f0bc',
     appId: '1:484816020469:web:b810508c4ea7c4aba1b6bc',
     databaseURL: 'https://films-2f0bc-default-rtdb.firebaseio.com',
     storageBucket: 'films-2f0bc.appspot.com',
     locationId: 'europe-west',
     apiKey: 'AIzaSyBvVaCQW19mHbJRd0CdbGLtIKUajnwuZaw',
     authDomain: 'films-2f0bc.firebaseapp.com',
-    messagingSenderId: '484816020469',
+    messagingSenderId: '484816020469', */
+    apiKey: "AIzaSyB1-SfuU4tz9zoycXYLw2bWPqPyWH3vHuA",
+    authDomain: "ionic-project-93930.firebaseapp.com",
+    projectId: "ionic-project-93930",
+    storageBucket: "ionic-project-93930.appspot.com",
+    messagingSenderId: "892583465986",
+    appId: "1:892583465986:web:211f689ee653fbb6e9031b",
+    measurementId: "G-BDR8R7YRKF"
   },
 };
 
